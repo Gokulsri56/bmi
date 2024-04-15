23eaaad8f16a8ffc18fda3bfd2fb3f8c785ab72b
Gokulsri56 <gokulgn454@gmail.com>
2024-04-15 10:23:30 +0530

    added BMI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from "react"
 
 function App() {
+  const [height,setHeight]=useState("")
+  const [weight,setWeight]=useState("")
+  const [bmi,setBmi]=useState("")
+  const [bmiState,setBmiState]=useState("")
+  const [errormessage,setErrormessage]=useState("")
+
+  const calBmi=()=>{
+    const isValiedHeight=/^\d+$/.test(height)
+    const isValiedWeight=/^\d+$/.test(weight)
+    if (isValiedHeight && isValiedWeight){
+      const heightInmeters=height/100
+      const bmiValue=weight/(heightInmeters*heightInmeters)
+      setBmi(bmiValue.toFixed(2))
+      if (bmiValue<18.5){
+        setBmiState("Underweight")
+      }
+      else if(bmiValue>=18.5 &&  bmiValue<24.9){
+        setBmiState("Normal Weight")
+      }
+      else if(bmiValue>=25 &&  bmiValue<29.9){
+        setBmiState("Overweight")
+      }
+      else{
+        setBmiState("obese")
+      }
+      setErrormessage("")
+
+    }else{
+      setBmi(null);
+      setBmiState("")
+      setErrormessage("Please enter valied numeric values for height and weight")
+    }
+
+  }
+  const clearAll=()=>{
+    setHeight("")
+    setWeight("")
+    setBmi(null);
+      setBmiState("")
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="bmi-cal">
+      <div className="box"></div>
+      <div className="data">
+        <h1>BMI Calculator</h1>
+        {errormessage &&<p className='error'>{errormessage}</p>}
+        <div className="input-container">
+          <label htmlFot="height">Height (cm):</label>
+          <input type="text" id="height" value={height} onChange={(e)=>setHeight(e.target.value)}/>
+        </div>
+        <div className="input-container">
+          <label htmlFot="weight">Weight (kg):</label>
+          <input type="text" id="weight" value={weight}  onChange={(e)=>setWeight(e.target.value)}/>
+        </div>
+        <button onClick={calBmi}>Calculate BMI</button>
+        <button className='clear' onClick={clearAll}>Clear</button>
+         {bmi!==null &&(
+             <div className="result">
+             <p>Your BMI is :{bmi}</p>
+             <p>Status: {bmiState}</p>
+   
+           </div>
+         )}
+      </div>
     </div>
+    </>
   );
 }
 
